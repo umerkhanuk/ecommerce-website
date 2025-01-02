8c76b92c7dc3f7b6aa955865a164eea5970c3968
hp <uk129195@gmail.com>
2025-01-02 22:04:24 +0500

    Your commit message

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 
+// Define a Product type
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  thumbnail: string;
+};
+
 export default async function Home() {
   const fetchdata = await fetch("https://dummyjson.com/products");
   const response = await fetchdata.json();
+
+  // Explicitly type the response
+  const products: Product[] = response.products;
 
   return (
     <div className="grid items-center p-4 pb-5 gap-6 sm:p-10 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-2xl font-bold">Product List</h1>
       <ol className="flex flex-wrap gap-6">
-        {response.products.map((item: any) => (
+        {products.map((item) => (
           <li
             key={item.id}
             className="w-[24%] my-7 mx-1 max-h-[400px] text-center bg-slate-200 flex flex-col justify-center items-center gap-5 p-5"
